8df8f2d1103ca7936e79a723079c9ac32f1c484f
ngumina17 <nicole.gumina17@gmail.com>
2019-12-11 21:33:21 -0600

    answers and questions load onto first page

--- script.js
+++ script.js
@@ -8,8 +8,11 @@
 
 var startButton = document.getElementById("Begin")
 startButton.addEventListener('click', startGame)
-var nextQuestionButton = document.getElementById('Next')
+var nextQuestionButton = document.getElementById("Next")
 nextQuestionButton.addEventListener('click', nextQuestion)
+var questionContainerElement = document.getElementById("questionContainer")
+var triviaQuestion = document.getElementById("Question")
+var triviaAnswer = document.getElementById("answer-container")
 let questions = [
     //sources for questions: https://earth911.com/inspire/eco-quiz/
     // contd: https://uncw.edu/campuslife/documents/recyclingtriviaquestions.pdf
@@ -17,119 +20,142 @@ let questions = [
  {
     question: "What date is recognized as Earth Day in the United States and other parts of the world?",
     answers : [
-    {choice1: "April 22", correct: true},
-    {choice2: "January 15", correct: false},
-    {choice3: "June 26", correct: false},
-    {choice4: "November 9", correct: false}
+    {choice: "April 22", correct: true},
+    {choice: "January 15", correct: false},
+    {choice: "June 26", correct: false},
+    {choice: "November 9", correct: false}
     ]
  },
  
  {
     question: "Patagonia produces some of its apparel using polyester fabric that includes what upcycled items?",
     answers: [
-    {choice1: "Plastic bags", correct: false},
-    {choice2: "Fishing nets", correct: false},
-    {choice3: "Plastic bottles", correct: true},
-    {choice4: "Foam cups", correct: false}
+    {choice: "Plastic bags", correct: false},
+    {choice: "Fishing nets", correct: false},
+    {choice: "Plastic bottles", correct: true},
+    {choice: "Foam cups", correct: false}
     ]
  },
  {
     question: "How many trees does recycling a ton of paper save?",
     answers: [
-    {choice1: "6", correct: false},
-    {choice2: "17", correct: true},
-    {choice3: "100", correct: false},
-    {choice4: "75", correct: false}
+    {choice: "6", correct: false},
+    {choice: "17", correct: true},
+    {choice: "100", correct: false},
+    {choice: "75", correct: false}
     ]
  },
  {
      question: "The US Department of Energy (DOE) recommends turning off your monitor and placing the computer on sleep mode if you are not going to use it for more than ___",
      answers: [
-     {choice1: "2 hours", correct: false},
-     {choice2: "6 hours", correct: false},
-     {choice3: "45 minutes", correct: false},
-     {choice4: "20 minutes", correct: true}
+     {choice: "2 hours", correct: false},
+     {choice: "6 hours", correct: false},
+     {choice: "45 minutes", correct: false},
+     {choice: "20 minutes", correct: true}
      ]
  },
  
  {
     question: "Recycling plastic saves ____ times as much energy as burning it in an incinerator.",
     answers: [
-    {choice1: "2 times", correct: true},
-    {choice2: "0.5 times", correct: false},
-    {choice3: "3 times", correct: false},
-    {chocie4: "None, it takes more energy to recycle", correct: false}
+    {choice: "2 times", correct: true},
+    {choice: "0.5 times", correct: false},
+    {choice: "3 times", correct: false},
+    {chocie: "None, it takes more energy to recycle", correct: false}
     ]
  },
  
  {
      question: "How much energy does recycling just one aluminium can save?",
      answers: [
-     {choice1: "Enough to power a microwave for 30 minutes", correct: false},
-     {choice2: "Enough to power the average American home for 4 days", correct: false},
-     {choice3: "Enough to power a computer for 15 minutes", correct: false},
-     {choice4: "Enough to power a TV for 3 hours", correct: true}
+     {choice: "Enough to power a microwave for 30 minutes", correct: false},
+     {choice: "Enough to power the average American home for 4 days", correct: false},
+     {choice: "Enough to power a computer for 15 minutes", correct: false},
+     {choice: "Enough to power a TV for 3 hours", correct: true}
      ]
  },
  
  {
      question: "What type of supermarket bag is more ecofriendly, paper or plastic?",
      answers: [
-     {choice1: "Either is fine", correct: false},
-     {choice2: "Paper", correct: false},
-     {choice3:"Plastic", correct: false},
-     {choice4: "None of the above", correct: true}
+     {choice: "Either is fine", correct: false},
+     {choice: "Paper", correct: false},
+     {choice:"Plastic", correct: false},
+     {choice: "None of the above", correct: true}
      ]
  },
  
  {
     question: "Approximately how much global electricity output is produced from renewable sources?",
     answers :[
-    {choice1: "1 percent", correct: false},
-    {choice2: "10 percent", correct: true},
-    {choice3: "5 percent", correct: false},
-    {choice4: " 20 percent", correct: false}
+    {choice: "1 percent", correct: false},
+    {choice: "10 percent", correct: true},
+    {choice: "5 percent", correct: false},
+    {choice: " 20 percent", correct: false}
     ]
  },
  
  {
     question: "Dropping the thermostat from 70 to 68 degrees saves you about how much on your heating costs?",
     answers: [
-    {choice1: "10 percent", correct: false},
-    {choice2: "20 percent", correct: false},
-    {choice3: "1 percent", correct: false},
-    {choice4: "5 percent", correct: true}
+    {choice: "10 percent", correct: false},
+    {choice: "20 percent", correct: false},
+    {choice: "1 percent", correct: false},
+    {choice: "5 percent", correct: true}
     ]
  },
  
  {
     question: "Which of the following is an alternative material for making paper?",
     answers:[
-    {choice1: "Panda excrement", correct: false},
-    {choice2: "Hemp", correct: false},
-    {choice3: "Sheepskin", correct: false},
-    {choice4: "All of the above", correct: true}
+    {choice: "Panda excrement", correct: false},
+    {choice: "Hemp", correct: false},
+    {choice: "Sheepskin", correct: false},
+    {choice: "All of the above", correct: true}
     ]
  }
  ]
+let questionOrder, questionIndex
 
 //questions are hidden. once clicked, then first question appears. and the begin button disappears
 function startGame () {
     // console.log('click begin button')
 startButton.classList.add('hide')
-QAcontainerElement.classList.remove('hide')
-nextQuestion()
+questionOrder = questions.sort();
+questionIndex = 0
+questionContainerElement.classList.remove('hide')
+nextQuestion();
+
 } 
 
-
-
-
-// questions need to continue to populate
+// grab a question
 function nextQuestion() {
-    function loadNewQuestion()
+    loadNewQuestion(questionOrder[questionIndex]) 
+
+ }
+
+
+//display question and answer options in container. The next button also needs to show.
+function loadNewQuestion(question) {
+   triviaQuestion.innerText = question.question
+   nextQuestionButton.classList.remove('hide')
+   question.answers.forEach(answer => {
+      let answerButton = document.createElement('button')
+      answerButton.innerText = answer.choice
+      answerButton.classList.add('btn')
+      if (answer.correct) {
+         answerButton.dataset.correct = answer.correct
+      }
+      answerButton.addEventListener('click', chooseAnswer)
+      triviaAnswer.appendChild(answerButton)
+   });
+   chooseAnswer();
 }
 
 
 function chooseAnswer() {
+  
 
 }
+
+
